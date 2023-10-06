3e885d5357419e467da01589bda3a224707f0b04
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-05 18:09:56 -0700

    task5 in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("%s\n", "with proper grammar, but the outcome is a piece of art");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
 
