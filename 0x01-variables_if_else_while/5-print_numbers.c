91be4be2031a607381be7cda6e95228ef54015c9
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-07 05:57:33 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char out = 48;
+
+	while (out != 58)
+	{
+		putchar(out);
+		out++;
+	}
+	putchar('\n');
+	return (0);
+}
