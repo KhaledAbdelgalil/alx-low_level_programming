456b59f8f5ddcf723601383f140ab5748b68fd44
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-07 06:01:49 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char out = 97 + 25;
+
+	while (out != 96)
+	{
+		putchar(out--);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
