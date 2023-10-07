a459c7880336eaa97f2d602fd31d8c050cf18a23
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-07 07:50:17 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned short int out1 = 48;
+	unsigned short int out2;
+
+	while (out1 != 58)
+	{
+		out2 = out1 + 1;
+		while (out2 != 58)
+		{
+			putchar(out1);
+			putchar(out2++);
+			putchar(' ');
+		}
+		out1++;
+	}
+	putchar('\n');
+	return (0);
+}
