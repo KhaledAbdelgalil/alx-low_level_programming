49ea106235b091f34b42119718a5125053a10e6d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-10 05:39:54 -0700

    task12 in functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,11 +6,11 @@
  */
 void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
-		return;
 	char i, j;
 	char ans = 0;
-
+	
+	if (n > 15 || n < 0)
+		return;
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
