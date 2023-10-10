5166487ca8299148e264510de08f8a2fe5705f84
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-10 06:21:05 -0700

    task14 in functions

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int sum = 0;
+	unsigned long int first = 1, second = 1, temp = 2;
+
+	printf("%u", 1u);
+	while (temp <= 4000000 )
+	{
+		first = second;
+		second = temp;
+		sum += (temp % 2 == 0) ? temp : 0;
+		temp = first + second;
+	}
+
+	printf("%lu\n", sum);
+	return (0);
+}
