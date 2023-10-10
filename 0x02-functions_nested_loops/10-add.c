1a0379b4cf0a4bb80f900b6fe5db5e2753a68222
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-10 04:48:54 -0700

    task10 in functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ * @in1: first input
+ * @in2: second input
+ * Return: sum of in1 and in2
+ */
+int add(int in1, int in2)
+{
+	return (in1 + in2);
+}
