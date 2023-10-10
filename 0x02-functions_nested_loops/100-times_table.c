41bcae08b009857ae0c422af20badc4a3a4a212e
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-10 05:42:02 -0700

    task12 in functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,8 +40,10 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(ans / 10 + '0');
+				_putchar(ans / 100 + '0');
+				_putchar(ans % 100 + '0');
 				_putchar(ans % 10 + '0');
+				
 			}
 		}
 		_putchar('\n');
