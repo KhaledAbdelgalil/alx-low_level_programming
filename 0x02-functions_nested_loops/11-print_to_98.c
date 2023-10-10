3ebe6fe5894cc35a06d9fb0404daf8aa4a3c15f5
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-10 05:17:11 -0700

    task11 in functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,24 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n++ <= 98)
+		while (n <= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
 				printf(", ");
+			n++;
 		}
 		printf("\n");
 	}
 	else
 	{
-		while (n-- >= 98)
+		while (n >= 98)
 		{
 			printf("%d", n);
 
 			if (n == 98)
 				printf(", ");
+			n--;
 		}
 		printf("\n");
 	}
