ace941a1d1595e16b245a8be65ebe7cf1988c1d1
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-12 06:42:33 -0700

    task8 in more functions and loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  print_square - prints diagonal of length n, followed by new line.
+ *  @n: length of square
+ */
+void print_square(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	while (n > 0)
+	{
+		for (i = 0; i < spaces; i++)
+			_putchar('#');
+		putchar('\n');
+		n--;
+	}
+}
