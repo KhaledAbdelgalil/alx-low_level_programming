4859ff7619bd9b6634f2e5affe723a24bc5ff0de
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-12 06:53:12 -0700

    task9 in more functions and loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - fizzbuzz program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned char out = 1;
+
+	while (out != 101)
+	{
+		if (out % 15 == 0)
+			printf("FizzBuzz");
+		else if (out % 5 == 0)
+			printf("Buzz");
+		else if (out % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", out);
+		printf(" ");
+		out++;
+	}
+	printf("\n");
+	return (0);
+}
