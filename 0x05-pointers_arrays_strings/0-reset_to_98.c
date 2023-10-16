4c8e57d72c5a63265b4a1df333dc50d722616a1d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-16 00:53:17 -0700

    task0 in pointers/array tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *   reset_to_98 - reset input number to 98.
+ *
+ *  @input: pointer to input number
+ *  Return: void
+ */
+void reset_to_98(int *input)
+{
+	*input = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void _putchar(char);
+void reset_to_98(int);
+#endif /* MAIN_H */
