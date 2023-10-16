210c1ac4e56a28ce2e5c7fadd269a1d48f157f88
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-16 05:19:33 -0700

    task11 in pointers,arrays tasks

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,11 +6,11 @@ int main(void)
 {
     int remaining = 2772;
 
-    // Seed the random number generator
+    /* Seed the random number generator*/
     srand((unsigned int)time(NULL));
 
     while (remaining > 0) {
-        int r = rand() % 127 + 1; // Generate a random number between 1(ignore null char) and 127
+        int r = rand() % 127 + 1; /* Generate a random number between 1(ignore null char) and 127 */
         if (r > remaining) {
             r = remaining;
 	}
