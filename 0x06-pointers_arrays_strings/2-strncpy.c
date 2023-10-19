b9eb7c23264d0881321345cac03900b725af2059
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-18 17:21:39 -0700

    task2 in pointers, array,strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
+
+	while (i != n && src[i] != '\0') 
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
 	while (i != n)
 	{
-		dest[j] = src[i];
-		i++;
-		j++;
+		dest[i] = '\0';
 	}
 	return (dest);
 }
