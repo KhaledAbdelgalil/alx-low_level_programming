4bcbf1e91cbddfd230d1a661d0c9279d8600d9b1
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-18 18:26:13 -0700

    task5 in pointers, array,strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  string_toupper -  capatilize lower letters
+ *
+ *  @in: pointer to first char in string
+ *  Return: pointer to new string
+ */
+char* string_toupper(char *in)
+{
+	int cnt = 0;
+
+	while (in[cnt] != '\0')
+	{
+		if (in[cnt] >= 'a' && in[cnt] <= 'z')
+			in[cnt] = in[cnt] - 'a' + 'A'
+		cnt++;
+	}
+	return (in);
+}
