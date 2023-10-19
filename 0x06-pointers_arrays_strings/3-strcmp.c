7002678a915ec0b9e1a43d00deaf6465bfbb7f3f
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-18 17:56:00 -0700

    task3 in pointers, array,strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *   _strcmp - compare 2 strings
+ *
+ *  @s1: pointer to first char in 1st string
+ *  @s2: pointer to first char in 2nd string
+ *  Return: pointer to destination
+ */
+char *_strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int res = 0;
+
+	for (; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+	}
+	return (0);
+}
