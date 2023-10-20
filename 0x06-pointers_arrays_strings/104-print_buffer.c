17ea786ad7aa3c27395882bc3eb2ac9b4a9c2b1d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-19 19:21:21 -0700

    task12 in strings, arrays, pointers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,37 +8,39 @@
 void print_buffer(char *b, int size)
 {
 	int i, j, k;
-  	if (size <= 0)
-  	{
+
+	if (size <= 0)
+	{
 		printf("\n");
 		return;
 	}
-	for(i = 0; i < size; i = i + 10)
-	{
-		printf("%.8x:", i);
-		j = 0;
-		while(j < i + 10)
+		for (i = 0; i < size; i = i + 10)
 		{
-				if (j % 2 == 0)
+			printf("%.8x:", j);
+			k = j;
+			while (k < i + 10)
+			{
+				if (k % 2 == 0)
 					printf(" ");
-				if (j < size)
-					printf("%.2x", b[j]);
+				if (k < size)
+					printf("%.2x", *(b + k));
 				else
 					printf("  ");
-				j++;
-		}
-		printf(" ");
-		k = i;
-		while (k < i + 10)
-		{
-				if (k >= size)
+			}
+			printf(" ");
+			j = i;
+			while (j < i + 10)
+			{
+				if (l >= size)
 					break;
-				if (b[k] < 32 || b[k]> 126)/*characters from space (32) to tilde (126)*/
+				if (*(b + j) < 32 || *(b + j) > 126)
+				{
+					/*characters from space (32) to tilde (126)*/
 					printf("%c", '.');
+				}
 				else
-					printf("%c", b[k]);
-				k++;
-		}
+					printf("%c", *(b + j));
+			}
 			printf("\n");
-	}   
+		}
 }
