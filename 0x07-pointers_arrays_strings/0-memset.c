b75902f463a3105ae3b6d322b601ac8c1ca61512
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-23 09:15:54 -0700

    task0 in 0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: our memory
+ * @b: constant byte
+ * @b: the size of the memory to fill
+ *
+ * Return: pointer to destination.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+    
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+char *_memset(char *s, char b, unsigned int n);
+#endif /* MAIN_H */
