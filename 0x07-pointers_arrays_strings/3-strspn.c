4015666d1f152e4a0a5da393bfe8779985df9ce0
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-23 10:06:40 -0700

    task 3 in 0x07

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - calculates the number of bytes in the initial segment of s which consist only of bytes from accept
+ * @s: pointer to input string s
+ * accept: pointer to input valid bytes accept
+ *
+ * Return: number of bytes.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	bool stillValid = true;
+
+	i = 0;
+	while (s[i] != '\0' && stillValid)
+	{
+		stillValid = false;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				stillValid = true;
+				break;
+			}
+		}
+		
+	}
+	return (i);
+}
+
