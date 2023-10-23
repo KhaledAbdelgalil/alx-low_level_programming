d90d05d05e0e9591d1090cb0c395fddfa7dca1e7
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-23 11:48:03 -0700

    task 6 in 0x07

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - print chessboard
+ *
+ * @board: 2d chessboard 8*8
+ *
+ * Return: void , just print it
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned char i = 0, j = 0;
+
+	while (i != 8)
+	{
+		j = 0;
+		while ( j != 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
