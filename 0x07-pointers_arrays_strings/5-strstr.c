29a5f103a2cd190dbced25edc8a16b08dc159e3b
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-23 10:51:45 -0700

    task 5 in 0x07

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ *
+ * @s: pointer to input string s
+ * @accept: string to be located
+ *
+ * Return:  pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, startofWindow = 0;
+
+	while (s[i] != accept[j] && s[i] != '\0')
+	{
+		i++;
+		startofWindow++;
+	}
+
+	while (s[i] != '\0' && accept[j] != '\0')
+	{
+		if (s[i] == accept[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			j = 0;
+			startofWindow = i;
+			while (s[i] != accept[j] && s[i] != '\0')
+			{
+				i++;
+				startofWindow++;
+			}
+		}
+	}
+	return (accept[j] != '\0' ? NULL : &s[startofWindow]);
+}
+
