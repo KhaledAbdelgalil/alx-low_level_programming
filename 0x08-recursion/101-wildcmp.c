9e723a81755239b1c53a6a153f8167b4e429aad3
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-25 09:55:51 -0700

    task8 in 0x08

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,14 +9,14 @@ int wildcmp(char *s1, char *s2)
 {
 
 /*first case: we reached end of both strings ==> 1*/
-	if (s1[0] == '\0' && s2[0] == '\0')
+	if (*s1 == '\0' &&s2 == '\0')
 		return (1);
 
 /*second case: we reached end of s1, and s2 has char at end not * ==> 0*/
-	if (s1[0] == '\0' && s2[0] == '*' && s2[1] != '\0')
+	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
 		return (0);
 /*third case: 2 strings at current idx have same char, advance both*/
-	if (s1[0] == s2[0])
+	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 /*
@@ -24,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 *so take what in s1 and advance s1 or
 *may not take anychar so advance s2
 */
-	if (s2[0] == '*')
+	if (*s2 == '*')
 		return (wildcmp(s1 + 2, s2) || wildcmp(s1, s2 + 1));
 	return (0);
 }
