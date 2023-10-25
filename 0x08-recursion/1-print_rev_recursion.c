d0fac77b3456d93b599bc0e544989b0ae74b7e7f
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-25 07:45:01 -0700

    task1 in 0x08

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse order
+ *@s: pointer to first char in string
+ *
+ * Return: void just print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	putchar(*s);
+	_print_rev_recursion(s + 1);
+}
