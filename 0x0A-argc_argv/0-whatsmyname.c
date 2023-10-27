00ccc813dcd6a613c1211e4cfd38c42b6e6d3f7e
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-27 08:36:27 -0700

    task 0 in 0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: array of the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc __attribute__((unused)) , char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
