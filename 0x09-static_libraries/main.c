2ae78bd1106b6a2ff80a21345ace268aa50fdac7
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-10-27 07:53:47 -0700

    task1 in 0x09

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    _putchar(abs(-1) + 48);
+    _putchar(_abs(-1) + 48);
     _putchar('\n');
     return (0);
 }
