a9f0c8827b8a95cf23c793e1872b1ec876161a7f
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-02 10:21:38 -0700

    task 5 in 0x0C

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be checked
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	unsigned int i = 0;
+
+	while (*(s + i))
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -11,17 +29,11 @@
 int main(int argc, char *argv[])
 {
 	char *str1, *str2, firstNonZeroComes = 0;
-	int len1, len2, len, i, j;
-	int *result, carry, digit1, digit2;
+	int len1, len2, len, i, j, *result, carry, digit1, digit2;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	str1 = argv[1], str2 = argv[2];
-	len1 = strlen(str1);
-	len2 = strlen(str2);
+	if (argc != 3 || !is_digit(str1) || !is_digit(str2))
+		printf("Error\n"), exit(98);
+	str1 = argv[1], str2 = argv[2], len1 = strlen(str1), len2 = strlen(str2);
 	len = len1 + len2 + 1;
 	result = (int *) malloc(sizeof(int) * len);
 	if (!result)
@@ -45,18 +57,15 @@ int main(int argc, char *argv[])
 		result[i + j + 1] += carry;
 		i--;
 	}
-	i = 0;
-	while (i < len - 1)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			firstNonZeroComes = 1;
 		if (firstNonZeroComes)
 			putchar(result[i] + '0');
-		i++;
 	}
 	if (firstNonZeroComes == 0)
 		putchar('0');
-	putchar('\n');
-	free(result);
+	putchar('\n'), free(result);
 	return (0);
 }
