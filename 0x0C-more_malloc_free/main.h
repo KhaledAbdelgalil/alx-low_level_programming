0174a167e0ccd8770b189c9208bf2fd41409b1d9
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-02 11:41:10 -0700

    task 2 in 0x0C

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,4 +4,5 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
