ff25ecfc15c606a2a8a433223d94058e438f1267
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-06 07:49:30 -0800

    task 2 in 0x0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the C file
+ * that is compiled and produce that program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        printf("%s\n", __FILE__);
+        return (0);
+}
