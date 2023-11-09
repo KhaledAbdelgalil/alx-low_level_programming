8951f287c97b0ef0365132d7801e9e45e40c215c
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-09 07:10:16 -0800

    all tasks in varadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 
 		if (i != n - 1 && separator != NULL)
-				printf("%s", separator);
+			printf("%s", separator);
 	}
 	va_end(valist);
 	printf("\n");
