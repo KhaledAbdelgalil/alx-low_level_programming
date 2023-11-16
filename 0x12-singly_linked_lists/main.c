9e3525e58b4e41d4467a304216467ba6de01414b
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-16 08:22:17 -0800

    task 6 in 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    printf("Hello, ");
-    printf("Holberton\n");
+    printf("Hello, Holberton\n");
     return (0);
 }
--- 0x12-singly_linked_lists/main.s
+++ 0x12-singly_linked_lists/main.s
@@ -1,10 +1,9 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
 	.section	.rodata
 .LC0:
-	.string	"Hello, "
-.LC1:
-	.string	"Holberton"
+	.string	"Hello, Holberton"
 	.text
 	.globl	main
 	.type	main, @function
@@ -12,18 +11,15 @@ main:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	leaq	.LC1(%rip), %rdi
+	lea	rdi, .LC0[rip]
 	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
+	mov	eax, 0
+	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
