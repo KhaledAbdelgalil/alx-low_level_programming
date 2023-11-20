4a919dfb6640aa8fa44973c173d69b36a1e2a3bc
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-20 15:25:12 -0800

    task 1 in more-singly-linked-lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * listint_len - return count of elements of a list<int>.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	for (;h; h = h->next, nodes++);
+	return (nodes);
+}
