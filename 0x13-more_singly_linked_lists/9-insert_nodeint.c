be6f3bd6be3ff73778a048b83def3720429bc94b
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-21 03:15:15 -0800

    task 9 in more singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newNode, *tmp;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -12,7 +12,7 @@ int main(void)
 {
     listint_t *head;
 
-    head = NULL;
+    head = NULL;/*
     add_nodeint_end(&head, 0);
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 2);
@@ -21,10 +21,11 @@ int main(void)
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
-    print_listint(head);
+    print_listint(head);*/
     printf("-----------------\n");
-    insert_nodeint_at_index(&head, 5, 4096);
+    insert_nodeint_at_index(&head, 0, 4096);
     print_listint(head);
     free_listint2(&head);
     return (0);
 }
+
