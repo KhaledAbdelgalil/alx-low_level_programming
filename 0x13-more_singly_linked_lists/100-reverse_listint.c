07ca82eb570907602f97123d05fec231eb16886a
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2023-11-21 05:27:28 -0800

    task 11 in more singly-linked-list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,19 +7,19 @@
  * Return: pointer to the first node.
  */
 listint_t *reverse_listint(listint_t **head)
-{       
-        listint_t* curr, *beforeCurr;
+{
+	listint_t *curr, *beforeCurr;
 
-        if (head == NULL)
-                return (NULL);
-        curr = *head;
-        beforeCurr = NULL;
-        while (curr)
-        {
-                (*head) = (*head)->next;
-                curr->next = beforeCurr;
-                beforeCurr = curr;
-                curr = *head;
-        }
+	if (head == NULL)
+		return (NULL);
+	curr = *head;
+	beforeCurr = NULL;
+	while (curr)
+	{
+		(*head) = (*head)->next;
+		curr->next = beforeCurr;
+		beforeCurr = curr;
+		curr = *head;
+	}
 	return (*head = beforeCurr);
-}    
+}
