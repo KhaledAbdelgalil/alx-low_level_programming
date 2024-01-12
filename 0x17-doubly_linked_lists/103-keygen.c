ac120c419f86f5ab14eddd21266bb7dbc8a234f1
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-11 16:50:49 -0800

    task 11 in 0x17

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -9,49 +9,43 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *code;
-	int user_len = strlen(argv[1]), idx, tmp_calculation;
-	int const and = 63;
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-	(void)argc;
-	code = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	tmp_calculation = (user_len ^ 59) & and;
-	password[0] = code[tmp_calculation];
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
 
-	tmp_calculation = 0;
-	for (idx = 0; idx < user_len; idx++)
-		tmp_calculation += argv[1][idx];
-	tmp_calculation = (tmp_calculation ^ 79) & and;
-	password[1] = code[tmp_calculation];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
 
-	tmp_calculation = 1;
-	for (idx = 0; idx < user_len; idx++)
-		tmp_calculation *= argv[1][idx];
-	tmp_calculation = (tmp_calculation ^ 85) & and;
-	password[2] = code[tmp_calculation];
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
 
-	tmp_calculation = argv[1][0];
-	for (idx = 1; idx < user_len; idx++)
+	tmp = 0;
+	for (i = 0; i < len; i++)
 	{
-		if (argv[1][idx] > tmp_calculation)
-			tmp_calculation = argv[1][idx];
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
 	}
-	srand(tmp_calculation ^ 14);
-	password[3] = code[rand() & and];
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
 
-	tmp_calculation = 0;
-	for (idx = 0; idx < user_len; idx++)
-		tmp_calculation += (argv[1][idx] * argv[1][idx]);
-	tmp_calculation = (tmp_calculation ^ 239) & and;
-	password[4] = code[tmp_calculation];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
 
-	for (idx = 0; idx < argv[1][0]; idx++)
-		tmp_calculation = rand();
-	tmp_calculation = (tmp_calculation ^ 229) & and;
-	password[5] = code[tmp_calculation];
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
 
 	password[6] = '\0';
 	printf("%s", password);
