6e2085d08a1c17e0620141c252b8088fd11b5510
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-15 08:52:26 -0800

    task 2 in 0x18

---
+++ 0x18-dynamic_libraries/implementation2.c
@@ -0,0 +1,21 @@
+
+long int add(long int a, long int b)
+{
+	return (a+b);
+}
+long int sub(long int a, long int b)
+{
+	return (a-b);
+}
+long int mul(long int a, long int b)
+{
+	return (a*b);
+}
+long int div(long int a, long int b)
+{
+	return (a/b);
+}
+long int mod(long int a, long int b)
+{
+	return (a%b);
+}
