f707a9a97413887658f3e6050629eb6cd83b74fc
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-15 08:42:45 -0800

    task 0 in 0x18

---
+++ 0x18-dynamic_libraries/implementation.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+
+int _putchar(char c){return 1;}
+int _islower(int c){return 1;}
+int _isalpha(int c){return 1;}
+int _abs(int n){return 1;}
+int _isupper(int c){return 1;}
+int _isdigit(int c){return 1;}
+int _strlen(char *s){return 1;}
+void _puts(char *s){}
+char *_strcpy(char *dest, char *src){return NULL;}
+int _atoi(char *s){return 1;}
+char *_strcat(char *dest, char *src){return NULL;}
+char *_strncat(char *dest, char *src, int n){return NULL;}
+char *_strncpy(char *dest, char *src, int n){return NULL;}
+int _strcmp(char *s1, char *s2){return 1;}
+char *_memset(char *s, char b, unsigned int n){return NULL;}
+char *_memcpy(char *dest, char *src, unsigned int n){return NULL;}
+char *_strchr(char *s, char c){return NULL;}
+unsigned int _strspn(char *s, char *accept){return 1;}
+char *_strpbrk(char *s, char *accept){return NULL;}
+char *_strstr(char *haystack, char *needle){return NULL;}
+
+
