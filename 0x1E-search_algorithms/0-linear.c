084b68ad524d9183d38ccd232b797fe650f9344a
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-06 13:25:05 +0100

    search algorimts complexity calculations in mandatory tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,27 +1,26 @@
 #include "search_algos.h"
 
 /**
- * linear_search - a function that performs
- * a linear search for a value in an array of integers
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
  *
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- * Return: first index if number found otherwise -1
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index or -1
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	int i;
 
 	if (array == NULL)
 		return (-1);
 
-	while (i < size)
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
 			return (i);
-		i++;
 	}
 	return (-1);
 }
