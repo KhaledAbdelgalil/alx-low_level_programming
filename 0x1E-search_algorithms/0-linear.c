43bae0039db3c153ab81b38360a3c4cf67c3d46b
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-06 13:12:54 +0100

    linear search2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,7 +7,6 @@
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
  * @value: the value to search for
- * Description: linear search
  * Return: first index if number found otherwise -1
  */
 int linear_search(int *array, size_t size, int value)
