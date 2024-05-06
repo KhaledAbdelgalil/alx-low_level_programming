ef4160a079370cc46a18af25fc6e7d54ea5012c3
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-06 19:36:07 +0100

    skip list

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in a skip list
+ *
+ * @list: input list
+ * @value: value to search in
+ * Return: index of the number
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *list_jump_end;
+
+	if (list == NULL)
+		return (NULL);
+	list_jump_end = list;
+	do {
+		list = list_jump_end;
+		list_jump_end = list_jump_end->express;
+		printf("Value checked at index ");
+		printf("[%u] = [%d]\n", list_jump_end->index, list_jump_end->n);
+	} while (list_jump_end->express && list_jump_end->n >= value);
+	if (list_jump_end->express == NULL)
+	{
+		list = list_jump_end;
+		while (list_jump_end->next)
+			list_jump_end = list_jump_end->next;
+	}
+	printf("Value found between indexes ");
+	printf("[%u] and [%u]\n", list->index, list_jump_end->index);
+	while (list != list_jump_end->next)
+	{
+		printf("Value checked at index [%u] = [%d]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
+}
